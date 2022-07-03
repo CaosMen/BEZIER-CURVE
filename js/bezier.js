4a5c37c23ab1b3ddb4c4b16bd5b20b3669abe40e
Lucas Lemos <llcf@ic.ufal.br>
2022-07-03 18:40:34 -0300

    feat: casteljau's algorithm
    Added casteljau's algorithm to plot bezier curve

--- js/bezier.js
+++ js/bezier.js
@@ -23,9 +23,29 @@ class BezierCurve {
     ctx.beginPath();
     ctx.lineWidth =  3;
 
+    let coordsX = [this.pointA.x, this.controlA.x, this.controlB.x, this.pointB.x],
+        coordsY = [this.pointA.y, this.controlA.y, this.controlB.y, this.pointB.y];
+        
     ctx.moveTo(this.pointA.x, this.pointA.y);
-    ctx.bezierCurveTo(this.controlA.x, this.controlA.y, this.controlB.x, this.controlB.y, this.pointB.x, this.pointB.y);
+    this.bezierCurveTo(coordsX, coordsY, 10000, ctx);
 
     ctx.stroke();
+  }
+
+  casteljauAlgorithm(coords, i, j, t){
+    if(j == 0) return coords[i]
+    return (1 - t) * this.casteljauAlgorithm(coords, i, j - 1, t) + t * this.casteljauAlgorithm(coords, i + 1, j - 1, t)
+  }
+
+  bezierCurveTo(coordsX, coordsY, num_steps, ctx){
+    let n = coordsX.length,
+        i;
+    for(i = 0;i < num_steps;i++){
+      let t = i / (num_steps - 1),
+          x = this.casteljauAlgorithm(coordsX, 0, n - 1, t),
+          y = this.casteljauAlgorithm(coordsY, 0, n - 1, t);
+      
+      ctx.lineTo(x, y);
+    }
   }
 }
